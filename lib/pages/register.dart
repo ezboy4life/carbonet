58cbad34fd0addb047dc57dca8188a8367eb3a64
ezboy4life <82066433+ezboy4life@users.noreply.github.com>
2024-05-21 17:25:38 -0300

    Added register page
    Adicionado página de cadastro do usuário. Ainda não foi implementado
    nada na página, apenas adicionado o arquivo em '/lib/pages/register.dart'.

---
+++ lib/pages/register.dart
@@ -0,0 +1,18 @@
+import "package:carbonet/utils/app_colors.dart";
+import 'package:carbonet/utils/logger.dart';
+import 'package:carbonet/widgets/input_field.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => RegisterPageState();
+}
+
+class RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
