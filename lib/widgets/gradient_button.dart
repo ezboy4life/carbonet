672fe2e80885381cb4d2981b0f9bc1f81a706b47
ezboy4life <82066433+ezboy4life@users.noreply.github.com>
2024-05-22 21:39:49 -0300

    Added GradientButton
    Adicionado a classe GradientButton que é um botão simples que recebe 3
    argumentos: O texto que aparece em cima do botão (label), uma lista de
    cores que serão aplicadas no gradiente (buttonColors), e uma função do
    tipo 'void' (onPressed) que é ativada quando o botão é pressionado.

---
+++ lib/widgets/gradient_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class GradientButton extends StatelessWidget {
+  final String label;
+  final List<Color> buttonColors;
+  final void Function()? onPressed;
+
+  const GradientButton({
+    super.key,
+    required this.label,
+    required this.buttonColors,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: ShapeDecoration(
+        shape: const StadiumBorder(),
+        gradient: LinearGradient(
+          colors: buttonColors,
+        ),
+      ),
+      child: TextButton(
+        style: const ButtonStyle(
+          splashFactory: NoSplash.splashFactory,
+        ),
+        onPressed: onPressed,
+        child: Text(
+          label,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
