50c7f933d381ba59b22f2277d0fd5e4622ab0613
WarmMateTea <mateus.hirata22@gmail.com>
2024-08-30 15:27:00 -0300

    fix - listar refeição expandindo corretamente agora
    credo que complicação. talvez ter usado o expansionPanelList.radio tivesse me poupado uma dor de cabeça. enfim tá resolvido essa parte.
    agora só colocar mais coisa, tipo botão de remover (que n remove de verdade, tira no app mas desmarca o isActive no bd), filtragem, descrição melhor, melhorar o visual de tudo, etc.

--- lib/pages/listar_refeicoes.dart
+++ lib/pages/listar_refeicoes.dart
@@ -2,11 +2,14 @@ import 'package:carbonet/data/database/alimento_ingerido_dao.dart';
 import 'package:carbonet/data/database/refeicao_dao.dart';
 import 'package:carbonet/data/models/refeicao.dart';
 import 'package:carbonet/utils/app_colors.dart';
+import 'package:carbonet/utils/dao_procedure_coupler.dart';
 import 'package:carbonet/utils/logged_user_access.dart';
 import 'package:flutter/material.dart';
 
 class ListarRefeicoes extends StatefulWidget {
-  const ListarRefeicoes({super.key});
+  ListarRefeicoes({super.key});
+  List<bool> _isOpen = [];
+  bool _isOpenHasBeenInit = false;
 
   @override
   State<ListarRefeicoes> createState() => _ListarRefeicoesState();
@@ -23,9 +26,6 @@ class _ListarRefeicoesState extends State<ListarRefeicoes> {
 
   @override
   Widget build(BuildContext context) {
-
-    List<bool> _isOpen;
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.defaultAppColor,
@@ -35,18 +35,15 @@ class _ListarRefeicoesState extends State<ListarRefeicoes> {
           future: _futureRefeicoes, 
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-        
-              // TODO MUITO IRRITANTE 💢💢💢
-              // o treco não abre pq qdo eu clico no trem de expandir, ele POR ALGUMA RAZÃO MISTERIOSA trigga o rebuild a árvore toda (ou pelo menos partindo do nó que tem esse future builder; consequência: ele re-gera essa lista _isOpen, e fecha TODOS os trequinhos de novo. filho da puta. flutter pq CARALHOS vc tá rebuildando esse povo todo????) alternativa: usar o expansionPanelList.radio, que em tese só deixa ter um mano aberto a cada tempo então essa lógica em tese tá embutida nele. vou dormir e ver oq resolvo.
-              _isOpen = List.generate(snapshot.data!.length, (i) => false);
-              print('on creation:');
-              for (int i = 0; i < _isOpen.length; i++) {
-                print("_isOpen[$i]: ${_isOpen[i]}");
+              if (widget._isOpenHasBeenInit == false) {
+                widget._isOpen = List.generate(snapshot.data!.length, (i) => false);
+                widget._isOpenHasBeenInit = true;
               }
         
               return ExpansionPanelList(
                 children: List.generate(snapshot.data!.length, (i) {
                   return ExpansionPanel(
+                    canTapOnHeader: true,
                     headerBuilder: (context, isOpen) {
                       return ListTile(
                         title: Text("${snapshot.data![i].tipoRefeicao} - ${snapshot.data![i].data.toString()}"),
@@ -56,7 +53,7 @@ class _ListarRefeicoesState extends State<ListarRefeicoes> {
                       //TODO: melhorar esse futuro enfiado direto no futurebuilder
                       //title: Text("a fazer; não sei como gerar uma lista de conteúdo dinâmica sem usar futuro direto no futureBuilder aqui"),
                       title: FutureBuilder(
-                        future: AlimentoIngeridoDAO().getAlimentoIngeridoByRefeicao(snapshot.data![i].id), 
+                        future: DaoProcedureCoupler.getAlimentoIngeridoByRefeicaoFullData(snapshot.data![i].id), 
                         builder: (context, snapshot) {
                           if (snapshot.connectionState == ConnectionState.done) {
                             return Column(
@@ -64,19 +61,17 @@ class _ListarRefeicoesState extends State<ListarRefeicoes> {
                                   (e) => Text("${e.alimentoReferencia.nome} - ${e.qtdIngerida}g")
                                 ).toList(),
                             );
-                            //Text(snapshot.data!.toString());
                           } else {
                             return const CircularProgressIndicator();
                           }
                         }),
                     ),
-                    isExpanded: _isOpen[i],
+                    isExpanded: widget._isOpen[i],
                   );
                 }),
                 expansionCallback: (i, isOpen) {
                   setState(() {
-                    print("isOpen[$i]: $isOpen");
-                    _isOpen[i] = !isOpen;
+                    widget._isOpen[i] = isOpen;
                   });
                 },
               );
